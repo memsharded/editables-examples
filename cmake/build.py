ffb90123258714246adab19ac0058365470836a1
memsharded <james@conan.io>
2019-07-17 13:31:58 +0200

    working

--- cmake/build.py
+++ cmake/build.py
@@ -9,21 +9,22 @@ def run(cmd):
 shutil.rmtree("say/build", ignore_errors=True)
 shutil.rmtree("hello/build", ignore_errors=True)
 
+settings = "-s build_type=Debug -s arch=x86"
 try:
     with open("say/src/say.cpp", "r") as f:
         say = f.read()
 
     run("cd say && conan link . say/0.1@user/testing --layout=layout")
-    run("cd say && mkdir build && cd build && conan install ..")
+    run("cd say && mkdir build && cd build && conan install .. %s" % settings)
     run("cd say/build && conan build ..")
 
-    run("cd hello && mkdir build && cd build && conan install ..")
+    run("cd hello && mkdir build && cd build && conan install .. %s" % settings)
     run("cd hello/build && conan build ..")
     run("cd hello/build/bin && app")
 
     # Modify say 
     with open("say/src/say.cpp", "w") as f:
-        f.write(say.replace("Release", "******* Release ******** "))
+        f.write(say.replace("Release", "******* Release ******** ").replace("Debug", "***** Debug *****"))
     run("cd say/build && conan build ..")
 
     # Rebuild hello
